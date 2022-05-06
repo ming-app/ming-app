424255f6537f531ee747347537d1dc79336d9160
Kim, Hyukjoong <wangmir@gmail.com>
2022-05-06 18:14:05 +0900

    Firebase 관련 setting 및 debugging 용 root page setting
    1. firebase configuration 을 진행
    
    Check: https://firebase.google.com/docs/flutter/setup?hl=ko&platform=ios
    
    2. firebase auth 관련 import 진행
    
    check: https://firebase.flutter.dev/docs/auth/usage
    
    3. Debugging 용 root page 만듦
    
    debug 모드일 때만 첫 page가 debug root page로 setting 됨. 왼쪽 사이드에 drawer를 setting해놓고, 해당 page에 route가 가능한 page들을 넣고 debugging 용도로 사용 가능.
    
    Signed-off-by: Kim, Hyukjoong <wangmir@gmail.com>

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ming/ui/screens/pet_feed_page.dart';
 
+import 'ui/screens/debug_root_page.dart';
+
 final routes = {
-  '/': (BuildContext context) => const PetFeedPage(),
+  '/debug_root' : (BuildContext context) => const DebugRootPage(),
+  '/feed': (BuildContext context) => const PetFeedPage(),
 };
