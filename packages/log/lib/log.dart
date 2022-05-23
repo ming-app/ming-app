d7f01a45d6b34ebe563d7851215378dc808363c4
Hyukjoong Kim <wangmir@gmail.com>
2022-05-24 00:38:14 +0900

    Log package fix: use includeCallerInfo rather than manual caller generation.
    Signed-off-by: Hyukjoong Kim <wangmir@gmail.com>

--- packages/log/lib/log.dart
+++ packages/log/lib/log.dart
@@ -6,7 +6,6 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_loggy/flutter_loggy.dart';
 import 'package:loggy/loggy.dart';
-import 'package:stack_trace/stack_trace.dart';
 
 class Log {
   static void init({bool firebaseEnable = true, bool verbose = false}) {
@@ -23,50 +22,16 @@ class Log {
       logOptions: LogOptions(
         kDebugMode ? (verbose ? LogLevel.all : LogLevel.debug) : LogLevel.info,
         stackTraceLevel: LogLevel.off,
+        includeCallerInfo: true,
+        callerFrameDepthLevel: 3,
       ),
     );
   }
 
-  static d(dynamic message, [Object? error, StackTrace? stackTrace]) =>
-      _log(message, LogLevel.debug, error, stackTrace);
-  static e(dynamic message, [Object? error, StackTrace? stackTrace]) =>
-      _log(message, LogLevel.error, error, stackTrace);
-  static i(dynamic message, [Object? error, StackTrace? stackTrace]) =>
-      _log(message, LogLevel.info, error, stackTrace);
-  static w(dynamic message, [Object? error, StackTrace? stackTrace]) =>
-      _log(message, LogLevel.warning, error, stackTrace);
-
-  static _log(dynamic message, LogLevel level,
-      [Object? error, StackTrace? stackTrace]) {
-    var prefix = "";
-
-    if (true) {
-      var debug = Trace.current();
-      var member = Trace.current().frames[2].member!.split(".");
-
-      // if there are no corresponding class, then class name will be ""
-      var className = member.length == 2 ? member[1] : "";
-      var methodName = member[0];
-      prefix = "$className/$methodName: ";
-    }
-
-    switch (level) {
-      case LogLevel.debug:
-        logDebug("$prefix$message", error, stackTrace);
-        break;
-      case LogLevel.error:
-        logError("$prefix$message", error, stackTrace);
-        break;
-      case LogLevel.info:
-        logInfo("$prefix$message", error, stackTrace);
-        break;
-      case LogLevel.warning:
-        logWarning("$prefix$message", error, stackTrace);
-        break;
-      default:
-        break;
-    }
-  }
+  static final d = logDebug;
+  static final e = logError;
+  static final w = logWarning;
+  static final i = logInfo;
 }
 
 String _logFormat(LogRecord record) {
@@ -78,9 +43,9 @@ String _logFormat(LogRecord record) {
   };
 
   final time = record.time.toIso8601String().split('T')[1];
-  final prefix = levelPrefixes[record.level] ?? "N";
+  final prefix = levelPrefixes[record.level] ?? "[N]";
 
-  return "$time: $prefix ${record.message}";
+  return "$time: $prefix ${record.callerFrame}: ${record.message}";
 }
 
 class _CustomPrinter extends LoggyPrinter {
