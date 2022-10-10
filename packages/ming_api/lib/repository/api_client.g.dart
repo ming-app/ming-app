e00dbbc12f5a4dde4781fae7a33b8192f5e47a11
Hyukjoong Kim <wangmir@gmail.com>
2022-10-10 18:46:56 +0900

    Fix refresh token api url bug.
    Signed-off-by: Hyukjoong Kim <wangmir@gmail.com>

--- packages/ming_api/lib/repository/api_client.g.dart
+++ packages/ming_api/lib/repository/api_client.g.dart
@@ -295,7 +295,7 @@ class _MingApiClient implements MingApiClient {
     final _result = await _dio.fetch<Map<String, dynamic>>(
         _setStreamType<AuthToken>(
             Options(method: 'GET', headers: _headers, extra: _extra)
-                .compose(_dio.options, '',
+                .compose(_dio.options, '/api/v1/users/token/refresh',
                     queryParameters: queryParameters, data: _data)
                 .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
     final value = AuthToken.fromJson(_result.data!);
